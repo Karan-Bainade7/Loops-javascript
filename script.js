561f1ae157014137c5b3b71c53ff2bc15e1d50ae
karan punamsing bainade <119426472+Karan-Bainade7@users.noreply.github.com>
2024-12-17 18:29:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+//loops
+//loops is used to pice of code execute again and again
+//loops can execute a block of code number of times.
+
+
+
+//print 5 number using for loops
+/
+// //print value of 20 to 50
+for(let  i=20; i<=50;i++){
+    console.log(i);
+}
+
+//calculate sum of 1 to 10
+a=0;
+for(let i=1;i<=5;i++){
+    sum=a+i;
+    
+}
+console.log(sum);
+
+//syntax of for loop
+for(i=2;i<=50;i++){
+    console.log("print value of i=",i);
+}
+
+
+//while loop
+let i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+//do while loop
+let i=10;
+do{
+    console.log("hello_karan");
+    i++;
+}while(i>20);
+//even condition is false then also one time execute of this code 
+
+//isi called do-while loop
+
+let i=0;
+do{
+    console.log("HELLO YOUR TARGET IS HIT TODAY'S");
+    i++;
+
+}
+
+let i=2;
+while(i<=10){
+    console.log("hello");
+    i++;
+}
+
+let i=1;
+do{
+    console.log("hello");
+    i++;
+}while(i<=3);
+
+
+
+//write even number print of 1 to 100
+    for(a=0;a<=100;a++){
+        if(a%2==0){
+            console.log("even number of 1 to 100=",a);
+        }
+    }
+
+
+
+ //write  odd number 1 to 100
+ for(sum=0; sum<=100;sum++){
+    if(sum%2  !==0){
+        console.log("your odd nnumber beetween 1 to 100 is=",sum);
+    }
+ }   
+
+ let game=143;
+ let user=  prompt("guess the game no.");
+while(game !==user){
+
+}
+console.log("congratulations you entered the right number");
+ 
+
